77e13a8961d507175399ee185ed470fbed60987d
Mohammad Emran <memran.dhk@gmail.com>
2025-08-02 21:18:58 +0600

    Update repository.

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,6 @@
 	use Marwa\Application\Input;
 	use Marwa\Application\Middlewares\AuthMiddleware;
 	Router::get('/', 'App\TestController::index');
+	Router::get('/version', function(){
+		return 'Version: ' . config('app.version');
+	});
