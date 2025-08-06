938e3f3ef96de7b65ad501802a736dd62686bb73
Mohammad Emran <memran.dhk@gmail.com>
2025-08-06 16:11:37 +0600

    Update composer package name.

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,11 @@
 
 	use Marwa\Application\Facades\Router;
 	use Marwa\Application\{Input,Response};
+	use Marwa\Application\Utils\Str;
 	use Marwa\Application\Middlewares\AuthMiddleware;
+
 	Router::get('/', 'App\TestController::index');
 	Router::get('/version', function(){
-		
-		logger("Version endpoint accessed");
-
-		return view('version', [
-			'title' => 'Version',
-			'version' => 1.0,
-			'date' => date('Y-m-d H:i:s')
-		]); 
+		$token = Str::password(10);
+		return Response::json(['Random String' => $token]);
 	});
