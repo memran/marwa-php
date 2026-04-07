fa6dfc965254688991d2c22dc1d75613addfff51
Mohammad Emran <memran.dhk@gmail.com>
2026-04-07 17:15:35 +0600

    Normalize module cache path

--- config/module.php
+++ config/module.php
@@ -5,7 +5,15 @@ declare(strict_types=1);
 return [
     'enabled' => env('MODULES_ENABLED', false),
     'paths' => [],
-    'cache' => (string) env('APP_MODULE_CACHE', cache_path('modules.php')),
+    'cache' => (function (): string {
+        $cache = (string) env('APP_MODULE_CACHE', cache_path('modules.php'));
+
+        if ($cache !== '' && $cache[0] !== DIRECTORY_SEPARATOR && !preg_match('/^[A-Za-z]:[\\\\\\/]/', $cache)) {
+            return base_path($cache);
+        }
+
+        return $cache;
+    })(),
     'forceRefresh' => in_array((string) env('APP_ENV', 'production'), ['local', 'development'], true),
     'commandPaths' => [
         'Console/Commands',
