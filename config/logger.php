75e2d21f8ae9f3c082accd58f3c738e2c8bb59c3
Mohammad Emran <memran.dhk@gmail.com>
2026-05-03 17:55:55 +0600

    fix: update Dockerfile and view templates
    Includes logger config update and welcome page tweaks.

--- config/logger.php
+++ config/logger.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'enable' => (bool) env('LOG_ENABLED', true),
+    'enable' => (bool) env('LOG_ENABLED', false),
     'filter' => [],
     'storage' => [
         'driver' => env('LOG_CHANNEL', 'file'),
